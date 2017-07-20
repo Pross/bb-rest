a5ceef03203505ee1bf585f74d6f66ddb70ce6f4
Simon Prosser <pross@unknown.home>
2017-07-20 20:50:10 +0100

    remove uneeded code

--- classes/class-fl-builder-rest.php
+++ classes/class-fl-builder-rest.php
@@ -8,27 +8,13 @@ final class FL_Rest{
 		}
 	}
 
-	public static function get_id( $request, $setup = true ) {
+	public static function get_id( $request ) {
 		global $post;
 		if( null !== $request->get_param( 'id' )){
 			$request_param_id = $request->get_param( 'id' );
-			$view = get_post( $request_param_id );
-			if( $view != null ){
-				$view_id = $view->ID;
-			} else {
-				$view_by_name = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM
-				$wpdb->posts WHERE post_name = %s", $request_param_id ) );
-				$view_id = $view_by_name->ID;
-			}
-
-			if( true === $setup ) {
-				if( ! is_object( $post ) ) {
-					$post = new stdClass;
-				}
-				$post->ID = $view_id;
-			}
+			$post = get_post( $request_param_id );
+			return $post->ID;
 		}
-		return $view_id;
 	}
 
 	static function strip_spaces( $txt ) {
